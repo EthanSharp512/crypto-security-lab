60917f360263bb1fbb423e034d132753c2f71ac2
Ethan Sharp <ethansharpdev@gmail.com>
2022-05-05 16:44:42 -0600

    added a object of code with entire alphabet equal to symbols

--- part-2-cipher/cipher.js
+++ part-2-cipher/cipher.js
@@ -1,15 +1,17 @@
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
-var newalpha = "";
+// var alphabet = "abcdefghijklmnopqrstuvwxyz";
+// var newalpha = "";
 
-function shift(n){
-	for (let i = 0; i < alphabet.length; i++){
-		let offset = (i + n) % alphabet.length;
-		newalpha += alphabet[offset];
-	}
-    console.log(newalpha)
-}
 
-shift(1)
+
+// function shift(n){
+// 	for (let i = 0; i < alphabet.length; i++){
+// 		let offset = (i + n) % alphabet.length;
+// 		newalpha += alphabet[offset];
+// 	}
+//     console.log(newalpha)
+// }
+
+// shift(1)
 
 // function encode(message){
 //     let result = "";
@@ -18,5 +20,38 @@ shift(1)
 //         let index = alphabet.indexOf(message[i]);
 //         result += newalpha[index];
 //     }
-//     return result;
+//     console.log(result);
 // }
+
+// encode("hello")
+
+
+
+let code = {
+    "a": "-",
+    "b": "_",
+    "c": "=",
+    "d": "+",
+    "e": "[",
+    "f": "]",
+    "g": "|",
+    "h": "}",
+    "i": ":",
+    "j": ";",
+    "k": "!",
+    "l": "@",
+    "m": "#",
+    "n": "$",
+    "o": "%",
+    "p": "^",
+    "q": "&",
+    "r": "*",
+    "s": "(",
+    "t": ")",
+    "u": "<",
+    "v": ">",
+    "w": ",",
+    "x": ".",
+    "y": "?",
+    "z": "/"
+}
