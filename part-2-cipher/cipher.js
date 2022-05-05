84bd8d35bd792737ea20653e17864d7e58aa156d
Ethan Sharp <ethansharpdev@gmail.com>
2022-05-05 16:24:20 -0600

    part 2 folder with cipher txt and cipher js

---
+++ part-2-cipher/cipher.js
@@ -0,0 +1,22 @@
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var newalpha = "";
+
+function shift(n){
+	for (let i = 0; i < alphabet.length; i++){
+		let offset = (i + n) % alphabet.length;
+		newalpha += alphabet[offset];
+	}
+    console.log(newalpha)
+}
+
+shift(1)
+
+// function encode(message){
+//     let result = "";
+//     message = message.toLowerCase();
+//     for (let i = 0; i < message.length; i++){
+//         let index = alphabet.indexOf(message[i]);
+//         result += newalpha[index];
+//     }
+//     return result;
+// }
