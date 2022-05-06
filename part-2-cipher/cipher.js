f553d8042fc0f8edf7d54098a6ee7fc475c51c80
Ethan Sharp <ethansharpdev@gmail.com>
2022-05-05 23:25:00 -0600

    problem 2 solved

--- part-2-cipher/cipher.js
+++ part-2-cipher/cipher.js
@@ -1,67 +1,79 @@
-let code = {
-    "a": "-",
-    "b": "_",
-    "c": "=",
-    "d": "+",
-    "e": "[",
-    "f": "]",
-    "g": "|",
-    "h": "}",
-    "i": ":",
-    "j": ";",
-    "k": "!",
-    "l": "@",
-    "m": "#",
-    "n": "$",
-    "o": "%",
-    "p": "^",
-    "q": "&",
-    "r": "*",
-    "s": "(",
-    "t": ")",
-    "u": "<",
-    "v": ">",
-    "w": ",",
-    "x": ".",
-    "y": "?",
-    "z": "/",
-    " ": "0"
-}
+// let code = {
+//     "a": "-",
+//     "b": "_",
+//     "c": "=",
+//     "d": "+",
+//     "e": "[",
+//     "f": "]",
+//     "g": "|",
+//     "h": "}",
+//     "i": ":",
+//     "j": ";",
+//     "k": "!",
+//     "l": "@",
+//     "m": "#",
+//     "n": "$",
+//     "o": "%",
+//     "p": "^",
+//     "q": "&",
+//     "r": "*",
+//     "s": "(",
+//     "t": ")",
+//     "u": "<",
+//     "v": ">",
+//     "w": ",",
+//     "x": ".",
+//     "y": "?",
+//     "z": "/",
+//     " ": "0"
+// }
 let alphabet = "abcdefghijklmnopqrstuvwxyz ";
 let newalpha = "";
 let result = "";
 
+function shift(num) {
+	for (let i = 0; i < alphabet.length; i++){
+		let offset = (i + num) % alphabet.length;
+		newalpha += alphabet[offset];
+	}
+}
+
 function encode(message, number) {
 
-    
-    for (let i = 0; i < alphabet.length; i++){
-        let offset = (i + number) % alphabet.length;
-        newalpha += alphabet[offset];
-    }
-    
+    shift(number);
+
     message = message.toLowerCase();
     for (let i = 0; i < message.length; i++){
         let index = alphabet.indexOf(message[i]);
         result += newalpha[index];
     }
-    
-    inverse(code) {
-        let retobj = {};
-        for(var key in obj){
-          retobj[code[key]] = key;
-        }
-        console.log(retobj);
-    }
+    console.log(result);
 }
 
+encode("I love cryptography", 3);
 
-encode("Hello Ethan", 1)
+// function secondEncoder(result){
+//     let secondEncoded = "";
+//     let dictionary = {};
 
+//     for(let i = 0; i < alphabet.length;i++){
+//         dictionary[newalpha[i]] = code[i];
+//     }
 
+//     for (let i = 0; i < result.length; i++) {
+//         let char = result.charAt(i);
+        
+//         if(dictionary.hasOwnProperty(char)){
+//             secondEncoded += dictionary[char];
+//         }else{
+//             secondEncoded += char;
+//         }
+//     }
 
-// function shift(num) {
-// 	for (let i = 0; i < alphabet.length; i++){
-// 		let offset = (i + num) % alphabet.length;
-// 		newalpha += alphabet[offset];
-// 	}
+//     return secondEncoded;
 // }
+
+// console.log(encode("Hello Ethan", 1))
+// secondEncoder();
+
+
